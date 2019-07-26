ae1dd146a7e35cb1a6e929fb82cd3f9c7d92569a
Sabina Chen <sabinach@mit.edu>
2019-07-25 23:46:40 -0400

    working video + image helper functions

--- utils.py
+++ utils.py
@@ -33,34 +33,41 @@ def show_frame(frame):
     PIL.Image.fromarray(frame).save(f, 'jpeg')
     img = IPython.display.Image(data=f.getvalue())
     display.update(img)
-    
-last_time = time.time()
-def staggered_video(frame, last_time):
-    #print(last_time, time.time())
-    if time.time() - last_time >= 1:
-        show_frame(frame)
-        last_time = time.time()
 
 #############################
 #### Identify Cone
 #############################
 
-def show_identified_image(colorRange, min_size, func):
+def find_greatest_contour(contours):
+    largest_contour = [-1, -1] 
+    for i, cnt in enumerate(contours):
+        if (cv2.contourArea(cnt) >= largest_contour[1]):
+            largest_contour  = [i, cv2.contourArea(cnt)]
+    return contours[largest_contour[0]]
+
+def show_identified_image(colorRange, min_size, func, save_image):
     global display
     display = IPython.display.display('', display_id=1)
-    cap = cv2.VideoCapture(1)
+    cap = cv2.VideoCapture(2)
     frame = func(cap.read()[1], (colorRange, min_size))
+    if save_image:
+        print('saving image...')
+        timestr = time.strftime("%m%d%Y-%H%M%S.png")
+        cv2.imwrite('../pictures/' + timestr, frame)   
     show_frame(frame)
     cap.release()
 
-def show_identified_video(colorRange, min_size, func, rc):
+def show_identified_video(colorRange, min_size, func, time_limit, rc):
     global display
-    display = IPython.display.display('', display_id=1)
-    cap = cv2.VideoCapture(1)
-    rc.run(func, (colorRange, min_size), 10)
-    cap.release()
-    
-def get_moment(contour):
+    display = IPython.display.display('', display_id=2)
+    rc.run(func, (colorRange, min_size), time_limit)
+
+
+#############################
+#### Follow Cone
+#############################
+
+def get_moment(contour, res, screen_center):
     M = cv2.moments(contour)
     if M['m00'] != 0.0:
         cx = int(M['m10']/M['m00'])
@@ -73,14 +80,8 @@ def get_moment(contour):
             ratio = dist/screen_center
             res[1] = ratio*c 
             #print(dist, ratio, angle)
-
-#############################
-#### Display
-#############################
-
-def find_greatest_contour(contours):
-    largest_contour = [-1, -1] 
-    for i, cnt in enumerate(contours):
-        if (cv2.contourArea(cnt) >= largest_contour[1]):
-            largest_contour  = [i, cv2.contourArea(cnt)]
-    return contours[largest_contour[0]]
+            return res   
+    return [0, 0]
+    
+    
+    
