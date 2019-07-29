f34ae4f66aa635029a92761143515b1b19b8d359
Sabina Chen <sabinach@mit.edu>
2019-07-29 05:01:58 -0400

    updated utils.py

--- utils.py
+++ utils.py
@@ -8,17 +8,37 @@ import cv2
 import numpy as np
 
 # ROS 
-import rospy
-from rospy.numpy_msg import numpy_msg
-from sensor_msgs.msg import LaserScan
-from sensor_msgs.msg import Image
-from ackermann_msgs.msg import AckermannDriveStamped
+try:
+    import rospy
+    from rospy.numpy_msg import numpy_msg
+    from sensor_msgs.msg import LaserScan
+    from sensor_msgs.msg import Image
+    from ackermann_msgs.msg import AckermannDriveStamped
+except:
+    print('ROS is not installed')
 
 # iPython Display
 import PIL.Image
 from io import BytesIO
 import IPython.display
 import time
+
+# Used for HSV select
+import threading
+try:
+    import ipywidgets as widgets
+except:
+    print('ipywidgets is not installed')
+
+#############################
+#### Parameters
+#############################
+
+# Video Capture Port
+video_port = 2
+
+# Display ID
+current_display_id = 1 # keeps track of display id
 
 
 #############################
@@ -34,6 +54,7 @@ def show_frame(frame):
     img = IPython.display.Image(data=f.getvalue())
     display.update(img)
 
+    
 #############################
 #### Identify Cone
 #############################
@@ -46,43 +67,72 @@ def find_greatest_contour(contours):
             largest_contour  = [i, cv2.contourArea(cnt)]
     return contours[largest_contour[0]]
 
-def show_identified_image(func, save_image):
-    global display
-    display = IPython.display.display('', display_id=1)
-    cap = cv2.VideoCapture(2)
-    frame = func(cap.read()[1])
-    if save_image:
-        print('saving image...')
-        timestr = time.strftime("%m%d%Y-%H%M%S.png")
-        cv2.imwrite('../pictures/' + timestr, frame)   
+def show_video(func, time_limit, rc):
+    global display, current_display_id
+    display = IPython.display.display('', display_id=current_display_id)
+    current_display_id += 1
+    
+    rc.run(func, time_limit)
+
+def show_image(func):
+    global display, current_display_id
+    display = IPython.display.display('', display_id=current_display_id)
+    current_display_id += 1
+    
+    cap = cv2.VideoCapture(video_port)
+    frame = func(cap.read()[1])   
     show_frame(frame)
     cap.release()
 
-def show_identified_video(func, time_limit, rc):
-    global display
-    display = IPython.display.display('', display_id=2)
-    rc.run(func, time_limit)
+
+#############################
+#### Line Follow
+#############################
+
+def crop(img, top, bottom):
+    return img[top:bottom,:,:]
 
 
 #############################
-#### Follow Cone
+#### HSV Select
 #############################
 
-def get_moment(contour, res, screen_center):
-    speed, angle = res
-    M = cv2.moments(contour)
-    if M['m00'] != 0.0:
-        cx = int(M['m10']/M['m00'])
-        c = -.5
-        if speed < 0:
-            c = .5
-        ratio = 0 
-        dist = cx - screen_center*1.0
-        if abs(dist) > 20 and screen_center != 0:
-            ratio = dist/screen_center
-            angle = ratio*c 
-            return [speed, angle]   
-    return [0, 0]
+# Mask and display video
+def hsv_select_live(limit = 10, fps = 4):
+    global current_display_id
+    display = IPython.display.display('', display_id=current_display_id)
+    current_display_id += 1
     
+    # Create sliders
+    h = widgets.IntRangeSlider(value=[0, 179], min=0, max=179, description='Hue:', continuous_update=True, layout=widgets.Layout(width='100%'))
+    s = widgets.IntRangeSlider(value=[0, 255], min=0, max=255, description='Saturation:', continuous_update=True, layout=widgets.Layout(width='100%'))
+    v = widgets.IntRangeSlider(value=[0, 255], min=0, max=255, description='Value:', continuous_update=True, layout=widgets.Layout(width='100%'))
+    display.update(h)
+    display.update(s)
+    display.update(v)
     
+    # Live masked video for the thread
+    def show_masked_video():
+        video = cv2.VideoCapture(video_port)
+        start = time.time()
+        while time.time() - start < limit:
+            frame = video.read()[1]
+            if frame is not None:
+                hsv_min = (h.value[0], s.value[0], v.value[0])
+                hsv_max = (h.value[1], s.value[1], v.value[1])
+                frame = cv2.resize(frame, (320, 240))
+                frame = cv2.flip(frame, 1)
+                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                img_hsv = cv2.cvtColor(frame, cv2.COLOR_RGB2HSV)
+                mask = cv2.inRange(img_hsv, hsv_min, hsv_max)
+                img_masked = cv2.bitwise_and(frame, frame, mask = mask)
+                f = BytesIO()
+                PIL.Image.fromarray(img_masked).save(f, 'jpeg')
+                img_jpeg = IPython.display.Image(data=f.getvalue())
+                display.update(img_jpeg)
+                time.sleep(1.0 / fps)
+        video.release()
     
+    # Open video on new thread (needed for slider update)
+    hsv_thread = threading.Thread(target=show_masked_video)
+    hsv_thread.start()
